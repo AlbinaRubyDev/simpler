6fa683effccdf3f30677c2b497347ed57c6ba754
albina <albinarubydev@gmail.com>
2025-11-01 19:15:50 +0400

    feat: логирование готово

--- middleware/logger.rb
+++ middleware/logger.rb
@@ -9,16 +9,16 @@ class AppLogger
 
   def call(env)
     status, headers, body = @app.call(env)
-    message = collect(env)
+    message = collect(env, status, headers)
     @logger.info(message)
     [status, headers, body]
   end
 
   private
 
-  def collect(env)
+  def collect(env, status, headers)
     request = Rack::Request.new(env)
-    [request_line(request), handler_line(env), parameters_line, response_line].join("\n")
+    [request_line(request), handler_line(env), parameters_line(request.params), response_line(status, headers, env)].join("\n")
   end
 
   def request_line(request)
@@ -29,12 +29,12 @@ class AppLogger
     "Handler: #{env['simpler.controller'].class}##{env['simpler.action']}"
   end
 
-  def parameters_line
-    "Parameters: "
+  def parameters_line(params)
+    "Parameters: #{params}"
   end
 
-  def response_line
-    "Response: тлько это попадает?"
+  def response_line(status, headers, env)
+    "Response: #{status} [#{headers['Content-Type']}] #{env['simpler.controller'].name}/#{env['simpler.action']}.html.erb"
   end
 
 end
