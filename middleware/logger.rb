ae0a95d9521f826bf00186b82ea6edf7445c83c3
albina <albinarubydev@gmail.com>
2025-11-01 18:50:00 +0400

    feat: логирование в процессе разработки - 2

--- middleware/logger.rb
+++ middleware/logger.rb
@@ -8,8 +8,33 @@ class AppLogger
   end
 
   def call(env)
-    @logger.info(env)
-    @app.call(env)
+    status, headers, body = @app.call(env)
+    message = collect(env)
+    @logger.info(message)
+    [status, headers, body]
+  end
+
+  private
+
+  def collect(env)
+    request = Rack::Request.new(env)
+    [request_line(request), handler_line(env), parameters_line, response_line].join("\n")
+  end
+
+  def request_line(request)
+    "#{"\n"}Request: #{request.request_method} #{request.fullpath}"
+  end
+
+  def handler_line(env)
+    "Handler: #{env['simpler.controller'].class}##{env['simpler.action']}"
+  end
+
+  def parameters_line
+    "Parameters: "
+  end
+
+  def response_line
+    "Response: тлько это попадает?"
   end
 
 end
